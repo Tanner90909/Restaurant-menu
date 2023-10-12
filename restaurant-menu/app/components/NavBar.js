43626dc15e9208c3f077627aa89fa46222d6de8c
Tanner90909 <tmmcdaniel909@gmail.com>
2023-10-12 15:42:19 -0400

    more page layout

--- restaurant-menu/app/components/NavBar.js
+++ restaurant-menu/app/components/NavBar.js
@@ -1,11 +1,11 @@
 export default function NavBar(){
     return(
         <div>
-            <nav class="navbar sticky-top bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Menu</a>
-                    <a class="navbar-brand" href="#">About Us</a>
-                    <a class="navbar-brand" href="#">Contact Us</a>
+            <nav className="navbar sticky-top bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Menu</a>
+                    <a className="navbar-brand" href="#">About Us</a>
+                    <a className="navbar-brand" href="#">Contact Us</a>
                 </div>
             </nav>
         </div>
